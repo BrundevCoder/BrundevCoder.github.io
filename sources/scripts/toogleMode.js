944da8371b83f7c7d6a1fffa205b277a71d84d3d
BrundevCoder <brunodevdrums@gmail.com>
2026-02-25 18:45:40 +0000

    Enhance toggleMode.js for better button text handling
    Added button text update for dark/light mode toggle and initialized button text to moon icon.

--- sources/scripts/toogleMode.js
+++ sources/scripts/toogleMode.js
@@ -3,19 +3,30 @@
 const DEBUG = true;
 
 const btn = document.getElementById("btn-toogle");
+const btnText = document.getElementById("btn-span");
+
+const moon = "dark_mode";
+const sun = "light_mode";
 
 function toogle() {
   document.body.classList.toggle("dark");
   if (DEBUG) {
     if (document.body.className) {
       console.log(`Body Classes: ${document.body.className}`);
+      btnText.textContent = sun;
     }
     else {
       console.log("Body Classes: [Light Mode]")
+      btnText.textContent = moon;
     }
   }
 }
 
 btn.addEventListener("click", toogle);
+
+// when init, put moon icon
+setTimeout(() => {
+  btnText.textContent = moon;
+}, 0o100);
 
 // Writed by > (b) RunCode < Made By: Bruno
